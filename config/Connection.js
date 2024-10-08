b307730111ab99e02d5f9149dfe15d72e6c99e2d
Pedro Fiuza <pedrofiuza13@gmail.com>
2024-10-07 21:34:57 -0300

    conexao

--- config/Connection.js
+++ config/Connection.js
@@ -13,11 +13,11 @@ require('dotenv').config()
 
 const Connection = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
+    host: 'dpg-cs27lae8ii6s739d50gg-a',
     port:  '5432',
-    username: 'postgres',
-    password: '1234',
-    database: 'fs29'
+    username: 'fs29_jy9n_user',
+    password: 'zDWrQhQDUPYjNe57l5hgSbdPUYJl7GgS',
+    database: 'fs29_jy9n'
 })
 
 
